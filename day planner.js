1f8c8b708d2768aabb79b29bbc8c1679d68ccd04
RodEsme24 <60196598+RodEsme24@users.noreply.github.com>
2020-04-06 20:23:17 -0700

    Add files via upload

--- day planner.js
+++ day planner.js
@@ -1,3 +1,4 @@
+$( document ).ready(function() {
 let toDayEl = $("#currentDay");
 toDayEl.text(moment().format("LLLL"));
 let currentTime = 9;
@@ -18,14 +19,31 @@ for (i = 0; i < hours.length; i++) {
 }
 
 var saveBtn=$(".saveBtn")
-saveBtn.on("click", function(){
-  var index = $( ".saveBtn" ).index( this );
-  var inPut= $(".textinput").eq(index);
-  var value=inPut.val()
-  var name=inPut.attr("name");
- localStorage.setItem(name,value); 
+    saveBtn.on("click", function(){
+      //var index = $( ".saveBtn" ).index( this );
+      //var inPut= $(".textinput").eq(index);
+      var inPut = $(this).parent().find(".textinput").first(); //gets the text input element (go to parent of button, then search children for textinput class, and use first result)
+      var value=inPut.val()
+      var name=inPut.attr("name");
+      //Saves to local storage from web page
+      console.log("Saving " + name + " as " + value);
+     localStorage.setItem(name,value);
+    })
+    console.log("Page Ready");
+    //Loads values from local storage and puts them into the web page
+    document.getElementsByName("9am")[0].value = localStorage.getItem("9am");
+    document.getElementsByName("10am")[0].value = localStorage.getItem("10am");
+    document.getElementsByName("11am")[0].value = localStorage.getItem("11am");
+    document.getElementsByName("12pm")[0].value = localStorage.getItem("12pm");
+    document.getElementsByName("1pm")[0].value = localStorage.getItem("1pm");
+    document.getElementsByName("2pm")[0].value = localStorage.getItem("2pm");
+    document.getElementsByName("3pm")[0].value = localStorage.getItem("3pm");
+    document.getElementsByName("4pm")[0].value = localStorage.getItem("4pm");
+    document.getElementsByName("5pm")[0].value = localStorage.getItem("5pm");
+    //...
+});
 
-})
+
 
 
 
